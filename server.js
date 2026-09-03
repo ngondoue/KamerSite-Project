f8cbcfde22aaeb28625b0a4ef5ae8ac849b32425
Faith Bervely <ngondoue.bervel@ictuniversity.edu.cm>
2026-09-03 14:36:37 +0200

    create sign up and login api

--- server.js
+++ server.js
@@ -16,6 +16,39 @@ mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log(`MongoDB SUCCESFULLY CONNECTED`))
     .catch((err) => err.message)
 
+
+// signup endpoint
+app.post("/api/signup", async(req,res) => {
+    const {name, email, password} = req.body
+
+    if(!name || !email || !password){
+        res.status(401).json({success: false, message: "Name, email, password required"})
+    }
+    const existingUser = await User.findOne({ email })
+    if(existingUser){
+        return res.status(409).json({success: false, message: "User already exists"})
+    }
+    const newUser = new User({ name, email, password })
+    await newUser.save()
+    res.status(201).json({success: true, message: "User created successfully"})
+})
+
+//login endpoint
+app.post("/api/login", async(req,res) => {
+    const {email, password} = req.body
+
+    if(!email || !password){
+        return res.status(401).json({success: false, message: "Email and password required"})
+    }
+    const existingUser = await User.findOne({ email: email })
+    if(!existingUser){
+        return res.status(404).json({success: false, message: "User not found"})
+    }
+    if(existingUser.password !== password){
+        return res.status(401).json({success: false, message: "Invalid password"})
+    }
+    res.status(200).json({success: true, message: "Login successful"})
+})    
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
